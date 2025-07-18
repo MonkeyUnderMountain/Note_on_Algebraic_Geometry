0e8e63c297c426204cfdaa927f3fb0e9520851f0
tianle_laptop <loveandjustice@88.com>
2025-07-18 18:54:11 +0800

    add a loop in file MakeNewSection

--- MakeNewSection.bat
+++ MakeNewSection.bat
@@ -1,6 +1,7 @@
 @echo off
 setlocal enabledelayedexpansion
 
+:main_loop
 :: Get user input
 set /p "target_path=Enter target path (e.g., C:\Users\user\Desktop\Note_on_Algebra_Geometry\chapters\preliminary): "
 set /p "project_name=Enter section name: "
@@ -8,11 +9,11 @@ set /p "project_name=Enter section name: "
 :: Validate input
 if "%target_path%"=="" (
     echo Error: Target path cannot be empty
-    goto :end
+    goto :retry_prompt
 )
 if "%project_name%"=="" (
     echo Error: Project name cannot be empty
-    goto :end
+    goto :retry_prompt
 )
 
 :: Build full path
@@ -21,57 +22,62 @@ set "full_path=%target_path%\%project_name%"
 :: Create target folder
 if exist "%full_path%" (
     echo Error: Folder already exists - "%full_path%"
-    goto :end
+    goto :retry_prompt
 )
 mkdir "%full_path%" || (
     echo Error: Failed to create folder - "%full_path%"
-    goto :end
+    goto :retry_prompt
 )
 
 :: Copy template files (assuming template_files in script directory)
 echo Copying template files...
 xcopy /E /I /Q "%~dp0template\main.tex" "%full_path%" || (
     echo Error: File copy failed
-    goto :end
+    goto :retry_prompt
 )
 xcopy /E /I /Q "%~dp0template\text.tex" "%full_path%" || (
     echo Error: File copy failed
-    goto :end
+    goto :retry_prompt
 )
 xcopy /E /I /Q "%~dp0template\placeholder.jpg" "%full_path%" || (
     echo Error: File copy failed
-    goto :end
+    goto :retry_prompt
 )
-
 
 :: Create symbolic link (example: linking to common_resources in parent directory)
 echo Creating symbolic link...
 mklink "%full_path%\noteformyself.cls" "..\..\..\template\noteformyself.cls" || (
     echo Error: Failed to create symbolic link
     echo Note: May require administrator privileges
-    goto :end
+    goto :retry_prompt
 )
 mklink "%full_path%\notation.tex" "..\..\..\template\notation.tex" || (
     echo Error: Failed to create symbolic link
     echo Note: May require administrator privileges
-    goto :end
+    goto :retry_prompt
 )
 mklink "%full_path%\ref.bib" "..\..\..\template\ref.bib" || (
     echo Error: Failed to create symbolic link
     echo Note: May require administrator privileges
-    goto :end
+    goto :retry_prompt
 )
 
-:success
 echo Project structure created successfully:
 echo Path: %full_path%
 echo Contents:
 dir /b "%full_path%"
-echo Symbolic link: common_link -> ..\common_resources
- 
+
+// ...existing code...
+:retry_prompt
+echo.
+echo Choose an option:
+echo [R] Run again
+echo [Q] Quit
+choice /c RQ /n /m "Press R to run again or Q to quit: "
+if errorlevel 2 goto :end
+if errorlevel 1 goto :main_loop
+
 :end
 echo.
-echo Script execution completed.
-echo Press any key to close this window...
-pause >nul
+echo Goodbye!
 endlocal
